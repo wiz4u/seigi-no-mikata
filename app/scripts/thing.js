4f16412f696b8c84bf83ddab58f64a9dcda671c0
Ikuo Terado <eqobar@gmail.com>
2014-04-20 04:38:43 +0900

    Support pool in Thing

--- app/scripts/thing.js
+++ app/scripts/thing.js
@@ -4,16 +4,41 @@
     var Thing = function (parentElment) {
         var element = document.createElement('div');
         element.classList.add('thing');
-        element.classList.add('thing-invisible');
-
         this.element = element;
+
+        this.activate(false);
 
         if (parentElment) {
             parentElment.appendChild(this.element);
         }
+
+        Thing.pool.push(this);
     };
 
     Thing.MAX_NUMBER = 64;
+
+    Thing.pool = [];
+
+    Thing.prototype.activate = function(isActive) {
+        this.isActive = isActive;
+
+        if (isActive) {
+            this.element.classList.remove('thing-invisible');
+        } else {
+            this.element.classList.add('thing-invisible');
+        }
+    };
+
+    Thing.getInactiveThing = function() {
+        var pool = Thing.pool;
+        for (var i = 0; i < pool.length; i++) {
+            if (!pool[i].isActive) {
+                return pool[i];
+            }
+        }
+
+        return null;
+    };
 
     Thing.prototype.setPosition = function(x, y) {
         var style = this.element.style;
